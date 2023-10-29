344cff1a629843871231d34d95b6b40f546fb138
yotta_chow <z18230209297@163.com>
2023-10-29 19:47:42 +0800

    临时

--- app/src/main/java/com/jlu/drsmog/AboutActivity.java
+++ app/src/main/java/com/jlu/drsmog/AboutActivity.java
@@ -2,7 +2,10 @@ package com.jlu.drsmog;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class AboutActivity extends AppCompatActivity {
 
@@ -10,5 +13,24 @@ public class AboutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        ImageButton imageButton = findViewById(R.id.btn_back);
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 创建一个意图(Intent)来启动MainActivity
+                Intent intent = new Intent(AboutActivity.this, MainActivity.class);
+
+                // 为了确保MainActivity不被重复创建，我们设置以下标志
+                // 这将使MainActivity成为任务栈的顶部，并清除其他所有活动
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+
+                // 使用意图启动活动
+                startActivity(intent);
+
+                // 如果你想关闭当前活动(例如，如果当前活动是一个子活动或一个次要的界面)，可以添加以下行
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/com/jlu/drsmog/HistoryActivity.java
+++ app/src/main/java/com/jlu/drsmog/HistoryActivity.java
@@ -1,6 +1,9 @@
 package com.jlu.drsmog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
 import android.widget.ListView;
 import androidx.appcompat.app.AppCompatActivity;
 import com.jlu.drsmog.database.DatabaseHelper;
@@ -17,10 +20,27 @@ public class HistoryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history); // 请确保这是你的layout文件名
+        ImageButton imageButton = findViewById(R.id.btn_back);
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 创建一个意图(Intent)来启动MainActivity
+                Intent intent = new Intent(HistoryActivity.this, MainActivity.class);
+
+                // 为了确保MainActivity不被重复创建，我们设置以下标志
+                // 这将使MainActivity成为任务栈的顶部，并清除其他所有活动
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+
+                // 使用意图启动活动
+                startActivity(intent);
+
+                // 如果你想关闭当前活动(例如，如果当前活动是一个子活动或一个次要的界面)，可以添加以下行
+                finish();
+            }
+        });
 
         listView = findViewById(R.id.listView_history);
         dbHelper = new DatabaseHelper(this);
-
         loadData();
     }
 
@@ -32,7 +52,6 @@ public class HistoryActivity extends AppCompatActivity {
                 android.R.layout.simple_list_item_1, // 这是一个简单的列表项布局，你可以自定义
                 data
         );
-
         listView.setAdapter(adapter);
     }
 }
