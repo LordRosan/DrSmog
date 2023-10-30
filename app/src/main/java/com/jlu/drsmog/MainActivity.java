f04a3cea3bf2c6f256a6a91f9d47b89b4d0ce6d4
Azmat77 <13095386+azmat77@user.noreply.gitee.com>
2023-10-30 17:51:57 +0800

    修改Main+

--- app/src/main/java/com/jlu/drsmog/MainActivity.java
+++ app/src/main/java/com/jlu/drsmog/MainActivity.java
@@ -2,13 +2,47 @@ package com.jlu.drsmog;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button buttonCamera;
+    private Button buttonHistory;
+    private Button buttonAbout;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {  //拍照/上传跳转
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonCamera=findViewById(R.id.buttonCamera);
+        buttonCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,CamActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        buttonHistory=findViewById(R.id.buttonHistory);
+        buttonHistory.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,HistoryActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        buttonAbout=findViewById(R.id.buttonAbout);
+        buttonAbout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,AboutActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
