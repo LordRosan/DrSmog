6038ed9508de4f70e6b922eb5dd449bef34adfc7
黄一桐 <hytong2003@163.com>
2023-11-01 19:11:36 +0800

    Camera完成！

--- app/src/main/java/com/jlu/drsmog/CamActivity.java
+++ app/src/main/java/com/jlu/drsmog/CamActivity.java
@@ -22,8 +22,11 @@ import android.hardware.camera2.TotalCaptureResult;
 import android.hardware.camera2.params.StreamConfigurationMap;
 import android.media.Image;
 import android.media.ImageReader;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
+import android.provider.Settings;
 import android.util.Size;
 import android.view.Surface;
 import android.view.TextureView;
@@ -43,6 +46,9 @@ public class CamActivity extends AppCompatActivity {
 
     private static final int CAMERA_REQUEST_CODE = 100;
     private static final int READ_REQUEST_CODE = 200;
+    private static final int MANAGE_REQUEST_CODE = 1024;
+
+    private boolean isRefuse = false;
 
     final int PHOTO_HEIGHT = 1280; // 默认图片高度
     final int PHOTO_WIDTH = 720; // 默认图片宽度
@@ -60,6 +66,7 @@ public class CamActivity extends AppCompatActivity {
     private Size imageDimension;
     private CameraManager cameraManager;
     private File file;
+    private ImageReader reader;
 
     String imagePath;
 
@@ -79,6 +86,14 @@ public class CamActivity extends AppCompatActivity {
                     Toast.makeText(this, "Read permission is required!", Toast.LENGTH_LONG).show();
                 }
                 break;
+            case MANAGE_REQUEST_CODE:
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+                    if(Environment.isExternalStorageManager()) {
+                        isRefuse = false; // 授权成功
+                    } else {
+                        isRefuse = true; // 授权失败
+                    }
+                }
             default:
         }
     }
@@ -87,6 +102,14 @@ public class CamActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cam);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R && !isRefuse) {
+            if (!Environment.isExternalStorageManager()) {
+                Intent intent = new Intent(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION);
+                intent.setData(Uri.parse("package:" + getPackageName()));
+                startActivityForResult(intent, 1024);
+            }
+        }
 
         cameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
 
@@ -120,52 +143,51 @@ public class CamActivity extends AppCompatActivity {
                 }
                 List<Surface> outputSurfaces;
                 final CaptureRequest.Builder captureBuilder;
-                try (ImageReader reader = ImageReader.newInstance(width, height, ImageFormat.JPEG, 1)) {
-                    outputSurfaces = new ArrayList<>(2);
-                    outputSurfaces.add(reader.getSurface());
-                    outputSurfaces.add(new Surface(cam_preview.getSurfaceTexture()));
+                reader = ImageReader.newInstance(width, height, ImageFormat.JPEG, 1);
+                outputSurfaces = new ArrayList<>(2);
+                outputSurfaces.add(reader.getSurface());
+                outputSurfaces.add(new Surface(cam_preview.getSurfaceTexture()));
 
-                    captureBuilder = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_STILL_CAPTURE);
-                    captureBuilder.addTarget(reader.getSurface());
-                    captureBuilder.set(CaptureRequest.CONTROL_MODE, CameraMetadata.CONTROL_MODE_AUTO);
+                captureBuilder = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_STILL_CAPTURE);
+                captureBuilder.addTarget(reader.getSurface());
+                captureBuilder.set(CaptureRequest.CONTROL_MODE, CameraMetadata.CONTROL_MODE_AUTO);
 
-                    imagePath = Environment.getExternalStorageDirectory() + "/image.jpg";
-                    file = new File(imagePath);
+                imagePath = Environment.getExternalStorageDirectory() + "/image.jpg";
+                file = new File(imagePath);
 
-                    ImageReader.OnImageAvailableListener readListener = new ImageReader.OnImageAvailableListener() {
-                        @Override
-                        public void onImageAvailable(ImageReader imageReader) {
-                            Image image = null;
-                            try {
-                                image = imageReader.acquireLatestImage();
-                                ByteBuffer buffer = image.getPlanes()[0].getBuffer();
-                                byte[] bytes = new byte[buffer.capacity()];
-                                buffer.get(bytes);
-                                save(bytes);
-                            } catch (IOException e) {
-                                e.printStackTrace();
-                            } finally {
-                                if (image != null) {
-                                    image.close();
-                                }
+                ImageReader.OnImageAvailableListener readListener = new ImageReader.OnImageAvailableListener() {
+                    @Override
+                    public void onImageAvailable(ImageReader imageReader) {
+                        Image image = null;
+                        try {
+                            image = imageReader.acquireLatestImage();
+                            ByteBuffer buffer = image.getPlanes()[0].getBuffer();
+                            byte[] bytes = new byte[buffer.capacity()];
+                            buffer.get(bytes);
+                            save(bytes);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        } finally {
+                            if (image != null) {
+                                image.close();
                             }
                         }
+                    }
 
-                        private void save(byte[] bytes) throws IOException {
-                            OutputStream outputStream = null;
-                            try {
-                                outputStream = new FileOutputStream(file);
-                                outputStream.write(bytes);
-                            } finally {
-                                if (outputStream != null) {
-                                    outputStream.close();
-                                }
+                    private void save(byte[] bytes) throws IOException {
+                        OutputStream outputStream = null;
+                        try {
+                            outputStream = new FileOutputStream(file);
+                            outputStream.write(bytes);
+                        } finally {
+                            if (outputStream != null) {
+                                outputStream.close();
                             }
                         }
-                    };
+                    }
+                };
 
-                    reader.setOnImageAvailableListener(readListener, null);
-                }
+                reader.setOnImageAvailableListener(readListener, null);
 
                 final CameraCaptureSession.CaptureCallback captureListener = new CameraCaptureSession.CaptureCallback() {
                     @Override
@@ -280,7 +302,6 @@ public class CamActivity extends AppCompatActivity {
 
                 @Override
                 public void onConfigureFailed(@NonNull CameraCaptureSession session) {
-                    Toast.makeText(CamActivity.this, "WHAT HAPPENED??", Toast.LENGTH_SHORT);
                 }
             }, null);
         } catch (CameraAccessException e) {
