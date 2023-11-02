40f71df7d422f0f49c64ff0788623ac221c214e1
yotta_chow <z18230209297@163.com>
2023-11-02 22:41:55 +0800

    历史记录成功显示

--- app/src/main/java/com/jlu/drsmog/HistoryActivity.java
+++ app/src/main/java/com/jlu/drsmog/HistoryActivity.java
@@ -1,57 +1,92 @@
 package com.jlu.drsmog;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
-import android.widget.ListView;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.jlu.drsmog.adapters.HistoryAdapter;
 import com.jlu.drsmog.database.DatabaseHelper;
+import com.jlu.drsmog.adapters.Record;
 
 import java.util.List;
 
 public class HistoryActivity extends AppCompatActivity {
 
-    private ListView listView;
+    private RecyclerView recyclerView;
     private DatabaseHelper dbHelper;
-    private List<String> data;
+    private HistoryAdapter adapter;
+    private List<Record> records;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_history); // 请确保这是你的layout文件名
+        setContentView(R.layout.activity_history);
+
         ImageButton imageButton = findViewById(R.id.btn_back);
         imageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // 创建一个意图(Intent)来启动MainActivity
                 Intent intent = new Intent(HistoryActivity.this, MainActivity.class);
-
-                // 为了确保MainActivity不被重复创建，我们设置以下标志
-                // 这将使MainActivity成为任务栈的顶部，并清除其他所有活动
                 intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
-
-                // 使用意图启动活动
                 startActivity(intent);
-
-                // 如果你想关闭当前活动(例如，如果当前活动是一个子活动或一个次要的界面)，可以添加以下行
                 finish();
             }
         });
-
-        listView = findViewById(R.id.listView_history);
+        // 初始化数据库
         dbHelper = new DatabaseHelper(this);
+        // 插入测试数据
+        dbHelper.addData("2023-10-28 12:00:00", "SomeBlacknessValue", "/some/path/to/data");
+        // 初始化 RecyclerView
+        recyclerView = findViewById(R.id.recyclerView_history);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
         loadData();
+        recyclerView.addOnItemTouchListener(new RecyclerTouchListener(this, recyclerView, new RecyclerTouchListener.ClickListener() {
+            @Override
+            public void onClick(View view, int position) {}
+
+            @Override
+            public void onLongClick(View view, int position) {
+                showDeleteDialog(position);
+            }
+        }));
     }
 
     private void loadData() {
-        data = dbHelper.getAllData();
+        records = dbHelper.getAllData();
+        if (records == null || records.isEmpty()) {
+            Log.d("HistoryActivity", "No data fetched from the database.");
+        } else {
+            Log.d("HistoryActivity", "Fetched " + records.size() + " records from the database.");
+        }
+        adapter = new HistoryAdapter(records);
+        recyclerView.setAdapter(adapter);
+        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(recyclerView.getContext(), DividerItemDecoration.VERTICAL);
+        recyclerView.addItemDecoration(dividerItemDecoration);
+    }
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(
-                this,
-                android.R.layout.simple_list_item_1, // 这是一个简单的列表项布局，你可以自定义
-                data
-        );
-        listView.setAdapter(adapter);
+    private void showDeleteDialog(final int position) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("删除条目");
+        builder.setMessage("您确定要删除此条目吗?");
+        builder.setPositiveButton("删除", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Record record = records.get(position);
+                dbHelper.deleteData(record.getId());
+                loadData();
+            }
+        });
+        builder.setNegativeButton("取消", null);
+        builder.show();
     }
 }
---
+++ app/src/main/java/com/jlu/drsmog/adapters/HistoryAdapter.java
@@ -0,0 +1,54 @@
+package com.jlu.drsmog.adapters;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.jlu.drsmog.R;
+
+import java.util.List;
+
+public class HistoryAdapter extends RecyclerView.Adapter<HistoryAdapter.ViewHolder> {
+    private List<Record> records;
+
+    public HistoryAdapter(List<Record> records) {
+        this.records = records;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_history, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        Record record = records.get(position);
+        holder.textViewTime.setText(record.getTime());
+        holder.textViewBlackness.setText(record.getBlackness());
+        holder.textViewPath.setText(record.getPath());
+    }
+
+    @Override
+    public int getItemCount() {
+        return records.size();
+    }
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+        TextView textViewTime;
+        TextView textViewBlackness;
+        TextView textViewPath;
+
+        public ViewHolder(View view) {
+            super(view);
+            textViewTime = view.findViewById(R.id.textView_time);
+            textViewBlackness = view.findViewById(R.id.textView_blackness);
+            textViewPath = view.findViewById(R.id.textView_path); // 注意这里ID仍然是imageView_path，尽管它是TextView
+        }
+    }
+}
